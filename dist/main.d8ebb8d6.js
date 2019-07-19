51fc99ba8f32c8bd2ffb59661f2a697644c7a46b
DRIMchansky <drimchansky@gmail.com>
2019-07-19 17:47:31 +0300

    ready to prod

--- dist/main.d8ebb8d6.js
+++ dist/main.d8ebb8d6.js
@@ -154,7 +154,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "7548" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "7440" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
