5bbd0f93ec25955abd4d2a346a32dba1d5ad7388
DRIMchansky <drimchansky@gmail.com>
2019-07-19 23:27:52 +0300

    re

--- dist/main.d2070829.js
+++ dist/main.d2070829.js
@@ -252,7 +252,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "11586" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "12002" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
