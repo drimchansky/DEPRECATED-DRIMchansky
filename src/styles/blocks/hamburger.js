0a315999a749673022b0121019460b2c1b878f33
DRIMchansky <drimchansky@gmail.com>
2019-07-27 18:46:32 +0300

    outline, keys

--- src/styles/blocks/hamburger.js
+++ src/styles/blocks/hamburger.js
@@ -4,9 +4,20 @@ const menu = document.querySelector('#menu');
 hamburger.addEventListener('click', function(){
     if (menu.classList.contains('visible')){
         this.classList.remove('is-active');
+        this.style.outline = 'none';
         menu.classList.toggle('visible', false);
+        
     } else {
         this.classList.add('is-active');
+        this.style.outline = '2px solid white';
         menu.classList.toggle('visible');
     }
 })
+
+document.addEventListener('keydown', function (e) {
+    if (e.keyCode === 27 ) {
+       menu.classList.remove('visible');
+       hamburger.classList.remove('is-active');
+       hamburger.style.outline = 'none';
+    }
+ })
