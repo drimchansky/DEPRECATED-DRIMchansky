477d3f48c0f472cf4bb6f02f4769c8f7c219986d
DRIMchansky <drimchansky@gmail.com>
2019-07-28 13:57:34 +0300

    new fonts, font-family

--- src/styles/blocks/hamburger.js
+++ src/styles/blocks/hamburger.js
@@ -2,12 +2,9 @@ const hamburger = document.querySelector('#hamburger');
 const menu = document.querySelector('#menu');
 
 hamburger.addEventListener('click', function(){
-    if (menu.classList.contains('visible')){
-        hamburger_close();
-    } else {
-        hamburger_open();
-    }
+    class_toggle();
 })
+/*
 const hamburger_close = () => {
     hamburger.classList.remove('is-active');
     hamburger.style.outline = 'none';
@@ -18,9 +15,15 @@ const hamburger_open = () => {
     hamburger.style.outline = 'none';
     menu.classList.toggle('visible');
 }
+*/
+    const class_toggle = () => {
+        hamburger.classList.toggle('is-active');
+        menu.classList.toggle('visible');
+        hamburger.style.outline = 'none';
+    }
 
-document.addEventListener('keydown', function (e) {
-    if (e.keyCode === 27 ) {
+document.addEventListener('keydown', function (press) {
+    if (press.keyCode === 27 ) {
     hamburger_close();
     }
  })
