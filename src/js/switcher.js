d76534fe62c1bc725b382bd7a5d63df3685f2542
DRIMchansky <drimchansky@gmail.com>
2019-08-17 02:54:13 +0300

    completely fix.. i want to sleep

--- src/js/switcher.js
+++ src/js/switcher.js
@@ -1,27 +1,29 @@
 const switcher = document.querySelector('.switcher');
 const switcherItem = document.querySelector('.switcher__item');
 const currentTheme = localStorage.getItem('theme');
-
+// eslint-disable-next-line func-names
+(function () {
 // set theme
-if (currentTheme) {
-    document.documentElement.setAttribute('data-theme', currentTheme);
-    if (currentTheme === 'dark') {
-        switcherItem.classList.add('switcher_night');
-    } else {
-        switcherItem.classList.remove('switcher_night');
+
+    if (currentTheme) {
+        if (currentTheme === 'day') {
+            document.documentElement.setAttribute('data-theme', 'day');
+            switcherItem.classList.remove('switcher_night');
+        } else {
+            document.documentElement.setAttribute('data-theme', 'dark');
+            switcherItem.classList.add('switcher_night');
+        }
     }
-}
+}());
+
 
 // switch theme
 function switchTheme() {
     if (switcherItem.classList.contains('switcher_night')) {
-        switcherItem.classList.remove('switcher_night');
         localStorage.setItem('theme', 'day');
         document.documentElement.setAttribute('data-theme', 'day');
         switcher.style.outline = 'none';
-
     } else {
-        switcherItem.classList.add('switcher_night');
         localStorage.setItem('theme', 'dark');
         document.documentElement.setAttribute('data-theme', 'dark');
         switcher.style.outline = 'none';
@@ -30,6 +32,7 @@ function switchTheme() {
 
 switcher.addEventListener('click', () => {
     switchTheme();
+    switcherItem.classList.toggle('switcher_night');
 });
 switcher.addEventListener('focus', () => switcher.style.outline = '1px solid var(--theme-primary)');
 switcher.addEventListener('blur', () => switcher.style.outline = 'none');
