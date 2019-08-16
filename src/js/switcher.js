4aa90e1af48ff8f7cdf546eac63bbeb4d00f775b
DRIMchansky <drimchansky@gmail.com>
2019-08-17 02:24:36 +0300

    switcher fix

--- src/js/switcher.js
+++ src/js/switcher.js
@@ -5,27 +5,29 @@ const currentTheme = localStorage.getItem('theme');
 // set theme
 if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
-    if (currentTheme === 'day') {
-        switcherItem.classList.remove('switcher_day');
+    if (currentTheme === 'dark') {
+        switcherItem.classList.add('switcher_night');
+    } else {
+        switcherItem.classList.remove('switcher_night');
     }
 }
 
 // switch theme
 function switchTheme() {
-    if (switcherItem.classList.contains('switcher_day')) {
-        document.documentElement.setAttribute('data-theme', 'dark');
+    if (switcherItem.classList.contains('switcher_night')) {
+        switcherItem.classList.remove('switcher_night');
         localStorage.setItem('theme', 'day');
-    } else {
         document.documentElement.setAttribute('data-theme', 'day');
+        switcher.style.outline = 'none';
+
+    } else {
+        switcherItem.classList.add('switcher_night');
         localStorage.setItem('theme', 'dark');
+        document.documentElement.setAttribute('data-theme', 'dark');
+        switcher.style.outline = 'none';
     }
 }
 
-// listners
-switcher.addEventListener('click', () => {
-    switcherItem.classList.toggle('switcher_day');
-    switcher.style.outline = 'none';
-});
 switcher.addEventListener('click', () => {
     switchTheme();
 });
