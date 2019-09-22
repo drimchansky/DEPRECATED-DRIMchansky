ca101425d2a2eac3ba3448374c8153fc925f10d3
DRIMchansky <drimchansky@gmail.com>
2019-09-22 23:19:16 +0300

    home page

---
+++ src/js/chrome-vh-fix.js
@@ -0,0 +1,6 @@
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+window.addEventListener('resize', () => {
+  vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
