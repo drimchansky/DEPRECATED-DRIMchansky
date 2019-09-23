2f696a293cd0484c30ee76fdfe17b6eb08ad32a5
DRIMchansky <drimchansky@gmail.com>
2019-09-23 03:10:11 +0300

    pug migration complete

--- src/js/hamburger.js
+++ src/js/hamburger.js
@@ -1,10 +1,12 @@
 const hamburger = document.querySelector('#hamburger');
 const menu = document.querySelector('#menu');
+const html = document.querySelector('html');
 
 const hamburgerToggle = () => {
   hamburger.classList.toggle('is-active');
   menu.classList.toggle('visible');
   hamburger.style.outline = 'none';
+  html.classList.toggle('scroll-disable');
 };
 
 // listners
