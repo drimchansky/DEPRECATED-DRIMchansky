933e445e37085c104c98d4e46ec06bccea471fd0
DRIMchansky <drimchansky@gmail.com>
2019-12-30 15:46:53 +0300

    Test with preloading sub-pages

--- src/js/chrome-vh-fix.js
+++ src/js/chrome-vh-fix.js
@@ -4,5 +4,4 @@ window.addEventListener('resize', () => {
   vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
 });
-
 document.documentElement.classList.remove('no-js');
