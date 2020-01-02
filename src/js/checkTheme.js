8197bc590a58ade9de9dcdf711623af87e079251
DRIMchansky <drimchansky@gmail.com>
2020-01-02 23:37:07 +0300

    New scripts, some js and css improvements

--- src/js/checkTheme.js
+++ src/js/checkTheme.js
@@ -1,12 +1,10 @@
-(function() {
-  const lightMode = localStorage.getItem('lightMode');
-  const html = document.querySelector('.page');
+const lightMode = localStorage.getItem('lightMode');
+const html = document.querySelector('.page');
 
-  if (lightMode === 'enabled') {
-    html.classList.add('light-mode');
-    localStorage.setItem('lightMode', 'enabled');
-  } else {
-    html.classList.remove('light-mode');
-    localStorage.setItem('lightMode', null);
-  }
-})();
+if (lightMode === 'enabled') {
+  html.classList.add('light-mode');
+  localStorage.setItem('lightMode', 'enabled');
+} else {
+  html.classList.remove('light-mode');
+  localStorage.setItem('lightMode', null);
+}
--- src/js/chrome-vh-fix.js
+++ src/js/chrome-vh-fix.js
@@ -1,7 +1,8 @@
+document.documentElement.classList.remove('no-js');
+
 let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 window.addEventListener('resize', () => {
   vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
 });
-document.documentElement.classList.remove('no-js');
