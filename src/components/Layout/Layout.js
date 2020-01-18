9570faf952ba2dfad669de6e7ee6053c0c135c45
DRIMchansky <drimchansky@gmail.com>
2020-01-18 15:42:07 +0300

    Add Layout component and playground graphQL

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import layoutStyles from './Layout.module.css'
+
+const Layout = () => {
+  return (
+    <div className={layoutStyles.layout}>
+      <></>
+    </div>
+  )
+}
+
+export default Layout
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,12 @@ export default () => {
   return (
     <Page>
       <h2>DRIMchansky</h2>
-      <p>Text</p>
+      <p>
+        Do sit deserunt consequat id velit non irure laboris. Non duis consequat commodo excepteur sunt laboris. Ad
+        voluptate duis fugiat sint nisi. Adipisicing reprehenderit magna proident cupidatat ut duis ex exercitation
+        eiusmod ullamco non ea sunt excepteur. Sunt qui cillum nulla proident culpa fugiat nostrud excepteur nisi Lorem
+        exercitation enim esse.
+      </p>
     </Page>
   )
 }
