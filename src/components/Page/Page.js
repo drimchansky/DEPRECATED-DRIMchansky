1861a5d03dacc88cf5253076326e69f0031b1cdd
DRIMchansky <drimchansky@gmail.com>
2020-01-19 13:26:16 +0300

    Add Toggl component

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import SiteMetadata from '../SiteMetadata'
 import Sidebar from '../Sidebar/Sidebar'
 import Hamburger from '../../components/Hamburger/Hamburger'
+import Toggl from '../../components/Toggl/Toggl'
 
 import styles from './Page.module.css'
 import '../../util/custom-normalize.css'
@@ -11,6 +12,11 @@ import '../../util/fonts.css'
 import '../../util/utility.css'
 
 const Page = props => {
+  // theme checking
+  useEffect(() => {
+    const theme = localStorage.getItem(theme)
+    console.log(theme)
+  })
   // hamburger state
   const [active, setActive] = useState(false)
 
@@ -21,6 +27,7 @@ const Page = props => {
         <Hamburger active={active} setActive={setActive} />
         <Sidebar active={active} setActive={setActive} />
         <>{props.children}</>
+        <Toggl />
       </div>
     </>
   )
