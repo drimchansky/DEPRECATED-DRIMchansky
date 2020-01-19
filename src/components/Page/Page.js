499d86c5846782883d4ff308af83ce0842ae4e66
DRIMchansky <drimchansky@gmail.com>
2020-01-19 20:35:33 +0300

    Fix chrome height

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -15,6 +15,15 @@ const Page = props => {
   // hamburger state
   const [active, setActive] = useState(false)
 
+  useLayoutEffect(() => {
+    let vh = window.innerHeight * 0.01
+    document.documentElement.style.setProperty('--vh', `${vh}px`)
+    window.addEventListener('resize', () => {
+      vh = window.innerHeight * 0.01
+      document.documentElement.style.setProperty('--vh', `${vh}px`)
+    })
+  })
+
   return (
     <>
       <SiteMetadata />
