a6c4e2351771c80377d1d599e9c13c270178a19b
DRIMchansky <drimchansky@gmail.com>
2020-01-19 19:27:10 +0300

    Add custom html.js, remove check theme hook

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -12,17 +12,6 @@ import '../../util/fonts.css'
 import '../../util/utility.css'
 
 const Page = props => {
-  //check theme
-  useLayoutEffect(() => {
-    const darkMode = localStorage.getItem('darkMode')
-    const body = document.body
-    if (darkMode === 'enabled') {
-      body.classList.add('dark')
-    } else {
-      body.classList.remove('dark')
-    }
-  })
-
   // hamburger state
   const [active, setActive] = useState(false)
 
