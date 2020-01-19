d800f5666a285d150c8b449cda8697956619d113
DRIMchansky <drimchansky@gmail.com>
2020-01-19 09:46:06 +0300

    Make logo as link

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 import Nav from '../Nav/Nav'
 import Social from '../Social/Social'
@@ -9,10 +10,10 @@ const Sidebar = () => {
   return (
     <aside className={styles.sidebar}>
       <div className={styles.wrapper}>
-        <p className={styles.title}>
+        <Link to="/" className={styles.title}>
           <span>DRIMchansky</span>
           <small>web addict</small>
-        </p>
+        </Link>
         <Nav />
       </div>
       <footer className={styles.footer}>
