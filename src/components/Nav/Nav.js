30ad8e12f43aec4ce54e71185aba37fc57745b6a
DRIMchansky <drimchansky@gmail.com>
2020-01-19 12:20:18 +0300

    Highlight current page

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -8,16 +8,24 @@ const Nav = () => {
     <nav className={styles.nav}>
       <ul className={styles.list}>
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/" activeClassName={styles.active}>
+            Home
+          </Link>
         </li>
         <li>
-          <Link to="/works">Works</Link>
+          <Link to="/works" activeClassName={styles.active}>
+            Works
+          </Link>
         </li>
         <li>
-          <Link to="/services">Services</Link>
+          <Link to="/services" activeClassName={styles.active}>
+            Services
+          </Link>
         </li>
         <li>
-          <Link to="/notes">Notes</Link>
+          <Link to="/notes" activeClassName={styles.active}>
+            Notes
+          </Link>
         </li>
       </ul>
     </nav>
