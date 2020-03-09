34e40e58bcbf392cc60fd1947a2aca8a5e04044c
Nikita Chernov <drimchansky@gmail.com>
2020-03-09 19:32:18 +0300

    Add SVG icons

--- src/components/works/WorkItem.js
+++ src/components/works/WorkItem.js
@@ -3,17 +3,20 @@ import Img from 'gatsby-image'
 
 import styles from './works.module.css'
 
+import CalendarIcon from '../icons/CalendarIcon'
+import LinkIcon from '../icons/LinkIcon'
+
 const WorkItem = ({ title, url, githubUrl, imageUrl, html, date }) => {
   return (
     <>
       <li className={styles.workItem}>
         <h2 dangerouslySetInnerHTML={{ __html: title }}></h2>
         <div className={styles.date}>
-          <div>image placeholder</div>
+          <CalendarIcon />
           <span>{date}</span>
         </div>
         <div className={styles.url}>
-          <div>image placeholder</div>
+          <LinkIcon />
           <a href={url}>Website</a>
         </div>
         <a href={githubUrl}>GitHub</a>
