52b120e744268c108885d07fd1f841c2c71747fd
Nikita Chernov <drimchansky@gmail.com>
2020-03-09 20:28:25 +0300

    Some styling

--- src/components/works/WorkItem.js
+++ src/components/works/WorkItem.js
@@ -5,22 +5,29 @@ import styles from './works.module.css'
 
 import CalendarIcon from '../icons/CalendarIcon'
 import LinkIcon from '../icons/LinkIcon'
+import GitHubIcon from '../icons/GitHubIcon'
 
 const WorkItem = ({ title, url, githubUrl, imageUrl, html, date }) => {
   return (
     <>
       <li className={styles.workItem}>
-        <h2 dangerouslySetInnerHTML={{ __html: title }}></h2>
-        <div className={styles.date}>
-          <CalendarIcon />
-          <span>{date}</span>
+        <h2 className={styles.header} dangerouslySetInnerHTML={{ __html: title }}></h2>
+        <div className={styles.info}>
+          <div className={styles.date}>
+            <CalendarIcon />
+            <span>{date}</span>
+          </div>
+          <div className={styles.url}>
+            <LinkIcon />
+            <a href={url}>Website</a>
+          </div>
         </div>
-        <div className={styles.url}>
-          <LinkIcon />
-          <a href={url}>Website</a>
-        </div>
-        <a href={githubUrl}>GitHub</a>
-        <p dangerouslySetInnerHTML={{ __html: html }}></p>
+        <p className={styles.text} dangerouslySetInnerHTML={{ __html: html }}></p>
+        <a className={styles.link} href={githubUrl}>
+          <GitHubIcon />
+          GitHub
+        </a>
+
         <Img className={styles.image} fluid={imageUrl} />
       </li>
     </>
