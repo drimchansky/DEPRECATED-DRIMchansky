2a10c56cd1e77290302a85f64b284e4cf53e9795
Nikita Chernov <drimchansky@gmail.com>
2020-04-01 13:06:15 +0300

    WorkItem from grid to flex

--- src/components/works/WorkItem.js
+++ src/components/works/WorkItem.js
@@ -11,27 +11,29 @@ const WorkItem = ({ title, tags, url, githubUrl, imageUrl, html, date, tech }) =
   return (
     <>
       <li className={styles.workItem} data-tech={tech}>
-        <h2 className={styles.header} dangerouslySetInnerHTML={{ __html: title }}></h2>
-        <div className={styles.info}>
-          <div className={styles.date}>
-            <CalendarIcon />
-            <span>{date}</span>
+        <div className={styles.wrapper}>
+          <h2 className={styles.header} dangerouslySetInnerHTML={{ __html: title }}></h2>
+          <div className={styles.info}>
+            <div className={styles.date}>
+              <CalendarIcon />
+              <span>{date}</span>
+            </div>
+            <div className={styles.url}>
+              <LinkIcon />
+              <a href={url}>Website</a>
+            </div>
           </div>
-          <div className={styles.url}>
-            <LinkIcon />
-            <a href={url}>Website</a>
-          </div>
+          <ul className={styles.tags}>
+            {tags.map((tag) => {
+              return <li>{tag}</li>
+            })}
+          </ul>
+          <span className={styles.text} dangerouslySetInnerHTML={{ __html: html }}></span>
+          <a className={styles.link} href={githubUrl}>
+            <GitHubIcon />
+            GitHub
+          </a>
         </div>
-        <ul className={styles.tags}>
-          {tags.map((tag) => {
-            return <li>{tag}</li>
-          })}
-        </ul>
-        <span className={styles.text} dangerouslySetInnerHTML={{ __html: html }}></span>
-        <a className={styles.link} href={githubUrl}>
-          <GitHubIcon />
-          GitHub
-        </a>
         <Img className={styles.image} fluid={imageUrl} />
       </li>
     </>
