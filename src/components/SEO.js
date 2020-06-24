5ada11e9f89daeb5c89de73c6f2684350a18a577
Nikita Chernov <drimchansky@gmail.com>
2020-06-24 09:45:56 +0300

    Update favicons

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -25,6 +25,14 @@ const SEO = ({ title, description }) => {
         <html lang="en" />
         <meta name="description" content={description || data.site.siteMetadata.description} />
         <meta name="theme-color" content="#191919" />
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="msapplication-TileColor" content="#00aba9" />
+
         <meta
           name="google-site-verification"
           content="s0KBDOaiFwczjIjfpI7nOgEx273026DfhIyK8WeN5AQ"
